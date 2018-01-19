327846a242941d6b4ae258ed4fa10dbebb7726d0
pawelztef <pawelztef@gmail.com>
2018-01-19 13:34:23 +0000

    deface cust

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_sidebar",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#sidebar[data-hook]",
-                     :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') Spree::BlogEntry %>",
-                     :disabled => false)
+                     # :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') Spree::BlogEntry %>",
+                       :text => "<p>Dont giva a s..</p>",
+                       :disabled => false)
