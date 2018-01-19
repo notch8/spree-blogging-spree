8a5e3e2d76798b03cf12044d453edd9b67081173
pawelztef <pawelztef@gmail.com>
2018-01-19 13:48:55 +0000

    path corrected

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#main-sidebar",
                      # :text => "<%= tab(:blog_entries, :label => 'Blog', :url => spree.admin_blog_entries_path, :icon => 'file') Spree::BlogEntry %>",
